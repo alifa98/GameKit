8b1243bfd2bea2f72e6f5738d4c22c31f2ef8543
Ali Faraji <billa.svpc@gmail.com>
2022-07-09 21:58:41 +0430

    the CEL for bankruptcy algorithm implemented (issue #3)

--- gamekit/algorithms/tests/test_bankruptcy.py
+++ gamekit/algorithms/tests/test_bankruptcy.py
@@ -4,7 +4,7 @@ from gamekit.algorithms.bankruptcy import *
 from gamekit import NegativeNumberException
 
 
-def test_two_person_equal():
+def test_constrained_equal_awards_two_person_equal():
     claims = [50, 60]
     asset = 50
     allocations, r = constrained_equal_awards(claims, asset)
@@ -13,7 +13,7 @@ def test_two_person_equal():
     assert r == 25
 
 
-def test_two_person_unequal():
+def test_constrained_equal_awards_two_person_unequal():
     claims = [50, 24]
     asset = 50
     allocations, r = constrained_equal_awards(claims, asset)
@@ -22,7 +22,7 @@ def test_two_person_unequal():
     assert r == 26
 
 
-def test_three_person_equal():
+def test_constrained_equal_awards_three_person_equal():
     claims = [50, 24, 68]
     asset = 60
     allocations, r = constrained_equal_awards(claims, asset)
@@ -31,7 +31,7 @@ def test_three_person_equal():
     assert r == 20
 
 
-def test_three_person_unequal():
+def test_constrained_equal_awards_three_person_unequal():
     claims = [50, 24, 68]
     asset = 75
     allocations, r = constrained_equal_awards(claims, asset)
@@ -39,7 +39,8 @@ def test_three_person_unequal():
     assert allocations == [25.5, 24, 25.5]
     assert r == 25.5
 
-def test_same_allocation():
+
+def test_constrained_equal_awards_same_allocation():
     claims = [50, 24, 68, 80]
     asset = 1000
     allocations, r = constrained_equal_awards(claims, asset)
@@ -48,15 +49,77 @@ def test_same_allocation():
     assert r == 80
 
 
-def test_non_negative_asset():
+def test_constrained_equal_awards_non_negative_asset():
     with pytest.raises(NegativeNumberException, match="Asset cannot be a negative number."):
         claims = [50, 24, 68]
         asset = -25
-        allocations, r = constrained_equal_awards(claims, asset)
+        constrained_equal_awards(claims, asset)
 
 
-def test_non_negative_claims():
+def test_constrained_equal_awards_non_negative_claims():
     with pytest.raises(NegativeNumberException, match="Claims cannot be a negative number."):
         claims = [50, -24, 68]
         asset = 25
-        allocations, r = constrained_equal_awards(claims, asset)
+        constrained_equal_awards(claims, asset)
+
+
+def test_constrained_equal_losses_two_person_equal_loss_1():
+    claims = [60, 90]
+    asset = 100
+    allocations, r = constrained_equal_losses(claims, asset)
+    assert allocations == [35, 65]
+    assert r == 25
+
+
+def test_constrained_equal_losses_two_person_equal_loss_2():
+    claims = [50, 100]
+    asset = 100
+    allocations, r = constrained_equal_losses(claims, asset)
+    assert allocations == [25, 75]
+    assert r == 25
+
+
+def test_constrained_equal_losses_two_person_equal_loss_3():
+    claims = [40, 80]
+    asset = 100
+    allocations, r = constrained_equal_losses(claims, asset)
+    assert allocations == [30, 70]
+    assert r == 10
+
+
+def test_constrained_equal_losses_two_person_unequal_loss():
+    claims = [450, 50]
+    asset = 100
+    allocations, r = constrained_equal_losses(claims, asset)
+    assert allocations == [100, 0]
+    assert r == 350
+
+
+def test_constrained_equal_losses_three_person_unequal_loss():
+    claims = [50, 100, 150]
+    asset = 100
+    allocations, r = constrained_equal_losses(claims, asset)
+    assert allocations == [0, 25, 75]
+    assert r == 75
+
+
+def test_constrained_equal_losses_three_person_same_allocation():
+    claims = [50, 100, 150]
+    asset = 300
+    allocations, r = constrained_equal_losses(claims, asset)
+    assert allocations == [50, 100, 150]
+    assert r == 0
+
+
+def test_constrained_equal_losses_non_negative_asset():
+    with pytest.raises(NegativeNumberException, match="Asset cannot be a negative number."):
+        claims = [50, 24, 68]
+        asset = -25
+        constrained_equal_losses(claims, asset)
+
+
+def test_constrained_equal_losses_non_negative_claims():
+    with pytest.raises(NegativeNumberException, match="Claims cannot be a negative number."):
+        claims = [50, -24, 68]
+        asset = 25
+        constrained_equal_losses(claims, asset)
